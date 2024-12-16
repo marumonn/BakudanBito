22b5851cf3d52513934402fceb816c0fa533031d
marumonn <soccerdf4513@gmail.com>
2024-12-16 14:38:08 +0900

    シーケンスのParentを少し追記

--- Sequence/Parent.cpp
+++ Sequence/Parent.cpp
@@ -1,4 +1,4 @@
-#include "GameLib/GameLib.h"
+﻿#include "GameLib/GameLib.h"
 #include "Sequence/Parent.h"
 #include "Sequence/Game/Parent.h"
 #include "Sequence/GameOver.h"
@@ -9,7 +9,7 @@ namespace Sequence {
 	Parent* Parent::mInstance = 0;
 
 	void Parent::create() {
-		ASSERT(!mInstance); //0������Ǝ~�܂�
+		ASSERT(!mInstance); //0が入ると止まる
 		mInstance = new Parent();
 	}
 
@@ -17,12 +17,12 @@ namespace Sequence {
 		ASSERT(mInstance);
 		SAFE_DELETE(mInstance);
 	}
-	//�V���O���g��
+	//シングルトン
 	Parent* Parent::instance() {
 		return mInstance;
 	}
-	//�����o�ϐ��̏�����
-	//�|�C���^�n�́A0�����Ă���
+	//メンバ変数の初期化
+	//ポインタ系は、0を入れている
 	Parent::Parent() :
 		mNextSequence(NEXT_NONE),
 		mMode(MODE_NONE),
@@ -30,12 +30,12 @@ namespace Sequence {
 		mGame(0),
 		mGameOver(0),
 		mEnding(0) {
-		//�ŏ��Ƀ^�C�g�������
+		//最初にタイトルを作る
 		mTitle = new Title();
 	}
 
 	Parent::~Parent() {
-		//�c���Ă���Ώ���
+		//残っていれば消す
 		SAFE_DELETE(mTitle);
 		SAFE_DELETE(mGame);
 		SAFE_DELETE(mGameOver);
@@ -46,5 +46,22 @@ namespace Sequence {
 		if (mTitle) {
 			mTitle->update(this);
 		}
+		else if (mGame) {
+			mGame->update(this);
+		}
+		else if (mGameOver) {
+			mGameOver->update(this);
+		}
+		else if (mEnding) {
+			mEnding->update(this);
+		}
+		else {
+			HALT("ありえないシーケンス");
+		}
+
+		//遷移判定
+		//面セレクトへ
+
+
 	}
 }
