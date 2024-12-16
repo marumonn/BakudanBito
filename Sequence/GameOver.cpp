860c38c35227098f4f9191e3c01fbf447fae9d33
marumonn <soccerdf4513@gmail.com>
2024-12-16 16:27:50 +0900

    GameOver.cpp実装

---
+++ Sequence/GameOver.cpp
@@ -0,0 +1,30 @@
+﻿#include "GameLib/GameLib.h"
+#include "GameLib/Framework.h"
+
+using namespace GameLib;
+
+#include "Sequence/GameOver.h"
+#include "Sequence/Parent.h"
+#include "Image.h"
+
+namespace Sequence {
+	GameOver::GameOver() : mImage(0), mCount(0) {
+		mImage = new Image("data/image/dummy.dds");
+	}
+
+	GameOver::~GameOver() {
+		SAFE_DELETE(mImage);
+	}
+
+	void GameOver::update(Parent* parent) {
+		if (mCount == 60) { //1秒待つ
+			parent->moveTo(Parent::NEXT_TITLE);
+		}
+		//描画
+		mImage->draw();
+		//字を出す
+		Framework::instance().drawDebugString(0, 0, "ｹﾞｰﾑｵｰﾊﾞｰ");
+
+		++mCount;
+	}
+}//namespace Sequence
