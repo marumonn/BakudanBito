afb85bd0425c3866980b9ca37ef166255bd81d73
marumonn <soccerdf4513@gmail.com>
2024-12-23 00:55:55 +0900

    File.h作成

--- File.h
+++ File.h
@@ -1,1 +1,15 @@
-#pragma once
+﻿#ifndef INCLUDED_FILE_H
+#define INCLUDED_FILE_H
+
+class File {
+public:
+	File(const char* filename);
+	~File();
+	int size() const;
+	const char* data() const;
+	unsigned getUnsigned(int position) const;
+private:
+	int mSize;
+	char* mData;
+};
+#endif
