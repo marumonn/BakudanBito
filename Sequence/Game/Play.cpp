10331fa93a5fa393c834329eb1cae71841f02768
marumonn <soccerdf4513@gmail.com>
2024-12-23 14:13:09 +0900

    GameのPlay.cpp実装完了

--- Sequence/Game/Play.cpp
+++ Sequence/Game/Play.cpp
@@ -18,7 +18,68 @@ namespace Sequence {
 
 		//ゲーム本体
 		void Play::update(Parent* parent) {
+			Framework f = Framework::instance();
 
+			State* state = parent->state();
+
+			bool cleared = state->hasCleared();
+			bool die1P = false;
+			bool die2P = false;
+			//とりあえずデバッグコマンドでテストする。
+			if (f.isKeyTriggered('1')) {
+				//1Pが殺す、つまり2Pが死ぬ
+				die2P = true;
+			}
+			else if (f.isKeyTriggered('2') || f.isKeyTriggered('x')) {
+				//2Pが殺す
+				die1P = true;
+			}
+			else if (f.isKeyTriggered('c')) {
+				cleared = true;
+			}
+
+			//スペースが押されたらポーズへ行く
+			if (f.isKeyTriggered(' ')) {
+				parent->moveTo(Parent::NEXT_PAUSE);
+			}
+
+			//クリアしたら上へ報告
+			if (parent->mode() == Parent::MODE_1P) {
+				if (cleared && !die1P) {
+					parent->moveTo(Parent::NEXT_CLEAR);
+				}
+				else if (die1P) {
+					parent->moveTo(Parent::NEXT_FAILURE);
+				}
+			}
+			else {
+				//二人用
+				if (die1P || die2P) {
+					//どっちかが死んでたら勝敗判定
+					parent->moveTo(Parent::NEXT_JUDGE);
+				}
+			}
+
+			//更新
+			int dx = 0;
+			int dy = 0;
+			if (f.isKeyOn('a')) {
+				dx -= 1;
+			}
+			else if (f.isKeyOn('s')) {
+				dx += 1;
+			}
+			else if (f.isKeyOn('w')) {
+				dy -= 1;
+			}
+			else if (f.isKeyOn('z')) {
+				dy += 1;
+			}
+
+			//更新
+			state->update(dx, dy);
+			//描画
+			state->draw();
 		}
 
 	}//namespace Game
