6010a1bdf0e41c2e63a0f51501a449a1aca3ce81
marumonn <soccerdf4513@gmail.com>
2024-12-24 20:01:56 +0900

    mainの作成

---
+++ main.cpp
@@ -0,0 +1,23 @@
+﻿#include "GameLib/Framework.h"
+using namespace GameLib;
+
+#include "Sequence/Parent.h"
+
+//ユーザ実装関数。中身はmainLoop()に丸投げ
+
+namespace GameLib {
+	void Framework::update() {
+		if (!Sequence::Parent::instance()) {
+			Sequence::Parent::create();
+			setFrameRate(60);
+		}
+		Sequence::Parent::instance()->update();
+		//終了判定( q が押されたか、マウスで×ボタンが押されたか
+		if (isKeyOn('q')) {
+			requestEnd();
+		}
+		if (isEndRequested()) {
+			Sequence::Parent::destroy();
+		}
+	}
+}//namespace GameLib
