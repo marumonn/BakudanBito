50d1526b29395ee27aeafa23fb2ea94066caec6c
marumonn <soccerdf4513@gmail.com>
2025-03-12 04:23:33 +0900

    描画の実装追加

--- Game/StaticObject.cpp
+++ Game/StaticObject.cpp
@@ -5,7 +5,11 @@ using namespace GameLib;
 #include "Image.h"
 
 //デフォルト壁。壁にしておけば抜けることはない。
-StaticObject::StaticObject() : mFlags(0) {
+StaticObject::StaticObject() : 
+	mFlags(0),
+	mBombOwner(0),
+	mCount(0)
+{
 }
 
 void StaticObject::SetFlag(unsigned f) {
@@ -31,8 +35,14 @@ void StaticObject::draw(int x, int y, const Image* image) const {
 		srcY = 16;
 	}
 	else if (mFlags & FLAG_BRICK) {
-		srcX = 0;
-		srcY = 32;
+		if (mFlags & (FLAG_FIRE_X | FLAG_FIRE_Y)) { //焼けてる
+			srcX = 0;
+			srcY = 48;
+		}
+		else {
+			srcX = 0;
+			srcY = 32;
+		}
 	}
 	else {
 		srcX = 16;
@@ -45,7 +55,11 @@ void StaticObject::draw(int x, int y, const Image* image) const {
 	//以下オプション描画
 	if (floor) {
 		srcX = -1; //番兵
-		if (mFlags & FLAG_ITEM_BOMB) {
+		if ((mFlags & FLAG_BOMB) && !(mFlags & FLAG_EXPLODING)) {
+			srcX = 32;
+			srcY = 32;
+		}
+		else if (mFlags & FLAG_ITEM_BOMB) {
 			srcX = 32;
 			srcY = 0;
 		}
@@ -56,5 +70,35 @@ void StaticObject::draw(int x, int y, const Image* image) const {
 		if (srcX != -1) {
 			image->draw(x * 16, y * 16, srcX, srcY, 16, 16);
 		}
+	}
+}
+
+void StaticObject::drawExplosion(int x, int y, const Image* image) const {
+	int srcX = -1;
+	int srcY = -1;
+	if (!(mFlags & FLAG_WALL) && !(mFlags & FLAG_BRICK)) { //壁の上には爆風は描かない
+		if (mFlags & FLAG_EXPLODING) {
+			srcX = 48;
+			srcY = 32;
+		}
+		else if (mFlags & FLAG_FIRE_X) {
+			if (mFlags & FLAG_FIRE_Y) {
+				//XもYも燃えてたら交差の画像にする
+				srcX = 48;
+				srcY = 32;
+			}
+			else {
+				srcX = 0;
+				srcY = 16;
+			}
+		}
+		else if (mFlags & FLAG_FIRE_Y) {
+			//XかつYは上で調べてる
+			srcX = 16;
+			srcY = 16;
+		}
+	}
+	if (srcX != -1) {
+		image->draw(x * 16, y * 16, srcX, srcY, 16, 16);
 	}
 }
